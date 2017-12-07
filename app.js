5482b2906915495e9caebf10ea77480c7dc9119e
Steven Lawson <unluckygamblermusic@gmail.com>
2017-12-07 11:09:16 -0700

    ready for review

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var minMaxButton = document.getElementById('min-max-button');
 var clearButton = document.querySelector('#clear-button');
-var clearClicked = document.getElementsByClassName('used')[0];
 var guessButton = document.querySelector('#guess-button');
 var resetButton = document.querySelector('#reset-button');
 var mainInput = document.querySelector('#main-input');
 var displayNumber = document.querySelector('.display-number');
 var highLow = document.querySelector('.high-low');
-var oneHundred = document.querySelector('.one-hundred');
-var fifty = document.querySelector('.fifty');
-var twentyFive = document.querySelector('.twenty-five');
-var boom = document.getElementById('boom-boom');
 var rangeButton = document.getElementById('range-button');
 var minNum = document.getElementById('min-num').value;
 var maxNum = document.getElementById('max-num').value;
@@ -20,7 +15,6 @@ function dynNum(){
   return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
 }
 var randomNum = dynNum();
-console.log(randomNum)
 
 rangeButton.addEventListener('click', function(event){
   event.preventDefault();
@@ -29,53 +23,78 @@ rangeButton.addEventListener('click', function(event){
   randomNum = dynNum();
 
 })
-
+function clearAgain(){
   mainInput.addEventListener('keyup', function(){
     clearButton.disabled = false;
     resetButton.disabled = false;
     });
+}
+clearAgain();
 
 clearButton.addEventListener('click',function(event){
+    clearAgain();
     event.preventDefault();
     var userGuess = mainInput.value;
     var usingGuess = parseInt(userGuess);
     form.reset();
     displayNumber.innerText = "?";
+    clearButton.disabled = true;
   });
 
 resetButton.addEventListener('click', function(event){
   event.preventDefault();
   form.reset();
+  clearAgain();
+  minNum.innerText = 0;
+  maxNum.innerText = 100;
   displayNumber.innerText = "?";
   highLow.innerText = "Please enter a number";
-  randomNum = fiftyRandomNum;
-  mainInput.addEventListener('keyup', function(){
-    clearButton.disabled = false;
-    resetButton.disabled = false;
-    });
+  resetButton.disabled = true;
 })
 guessButton.addEventListener('click', function(event){
-  event.preventDefault();
   var userGuess = mainInput.value;
   var usingGuess = parseInt(userGuess);
+  event.preventDefault();
   displayNumber.innerText = usingGuess;
+  display(usingGuess);
+  appending();
 
+console.log(minNum);
+console.log(maxNum);
+
+})
+function display(){
+  var userGuess = mainInput.value;
+  var usingGuess = parseInt(userGuess);
   if (usingGuess < randomNum){
-    var userGuess = mainInput.value;
-    var usingGuess = parseInt(userGuess);
     highLow.innerText = "Your Guess is too Low.";
   } else if (usingGuess > randomNum){
     highLow.innerText = "Your Guess is too High.";
   } else if (usingGuess === randomNum){
     highLow.innerText = "BOOM SHAKALA BOOM";
-    boom.style.hidden = false;
-    randomNum = newRandomNum;
   }
-
+}
+function appending (){
+  var userGuess = mainInput.value;
+  var usingGuess = parseInt(userGuess);
   if (isNaN(usingGuess)){
     displayNumber.innerText = "?";
     highLow.innerText = "Enter an actual number, bitte";
   } else if (usingGuess < 1 || usingGuess > 100){
     highLow.innerText = "Please enter a number in the given range";
   }
-})
+}
+function increaseDecrease () {
+  var userGuess = mainInput.value;
+  var usingGuess = parseInt(userGuess);
+  if (usingGuess === randomNum){
+    minNum = minNum - 10;
+    maxNum = maxNum + 10;
+    randomNum = randomNum > minNum && randomNum < maxNum;
+    console.log(randomNum)
+  } else if (usingGuess !== randomNum){
+    minNum = minNum + 10;
+    maxNum = maxNum - 10;
+    randomNum = randomNum > minNum && randomNum < maxNum;
+  }
+}
